1c7d14454fa87d8742f8bc215a5d0d0c9994c371
lipeng <lp960419@163.com>
2020-03-04 20:12:40 +0800

    commit

--- changgou_service/changgou_service_order/src/main/java/com/changgou/order/service/impl/OrderServiceImpl.java
+++ changgou_service/changgou_service_order/src/main/java/com/changgou/order/service/impl/OrderServiceImpl.java
@@ -33,6 +33,7 @@ public class OrderServiceImpl implements OrderService {
 
     /**
      * 查询全部列表
+     *
      * @return
      */
     @Override
@@ -42,12 +43,13 @@ public class OrderServiceImpl implements OrderService {
 
     /**
      * 根据ID查询
+     *
      * @param id
      * @return
      */
     @Override
-    public Order findById(String id){
-        return  orderMapper.selectByPrimaryKey(id);
+    public Order findById(String id) {
+        return orderMapper.selectByPrimaryKey(id);
     }
 
     @Autowired
@@ -73,11 +75,12 @@ public class OrderServiceImpl implements OrderService {
 
     /**
      * 增加
+     *
      * @param order
      */
     @Override
     @GlobalTransactional(name = "order_add")
-    public String add(Order order){
+    public String add(Order order) {
         //1.获取购物车的相关数据(redis)
         Map cartMap = cartService.list(order.getUsername());
         List<OrderItem> orderItemList = (List<OrderItem>) cartMap.get("orderItemList");
@@ -94,13 +97,13 @@ public class OrderServiceImpl implements OrderService {
         order.setOrderStatus("0"); //0:未完成 1:已完成 2:已退货
         order.setPayStatus("0"); //0:未支付 1:已支付
         order.setConsignStatus("0"); //0:未发货 1:已发货
-        String orderId = idWorker.nextId()+"";
+        String orderId = idWorker.nextId() + "";
         order.setId(orderId);
         orderMapper.insertSelective(order);
 
         //4.填充订单项数据并保存到tb_order_item
         for (OrderItem orderItem : orderItemList) {
-            orderItem.setId(idWorker.nextId()+"");
+            orderItem.setId(idWorker.nextId() + "");
             orderItem.setIsReturn("0"); //0:未退货 1:已退货
             orderItem.setOrderId(orderId);
             orderItemMapper.insertSelective(orderItem);
@@ -119,75 +122,80 @@ public class OrderServiceImpl implements OrderService {
         task.setMqRoutingkey(RabbitMQConfig.CG_BUYING_ADDPOINT_KEY);
 
         Map map = new HashMap();
-        map.put("username",order.getUsername());
-        map.put("orderId",orderId);
-        map.put("point",order.getPayMoney());
+        map.put("username", order.getUsername());
+        map.put("orderId", orderId);
+        map.put("point", order.getPayMoney());
         task.setRequestBody(JSON.toJSONString(map));
         taskMapper.insertSelective(task);
 
         //5.删除购物车数据(redis)
-        redisTemplate.delete("cart_"+order.getUsername());
+        redisTemplate.delete("cart_" + order.getUsername());
 
         //发送延迟消息
-        rabbitTemplate.convertAndSend("","queue.ordercreate",orderId);
+        rabbitTemplate.convertAndSend("", "queue.ordercreate", orderId);
         return orderId;
     }
 
 
     /**
      * 修改
+     *
      * @param order
      */
     @Override
-    public void update(Order order){
+    public void update(Order order) {
         orderMapper.updateByPrimaryKey(order);
     }
 
     /**
      * 删除
+     *
      * @param id
      */
     @Override
-    public void delete(String id){
+    public void delete(String id) {
         orderMapper.deleteByPrimaryKey(id);
     }
 
 
     /**
      * 条件查询
+     *
      * @param searchMap
      * @return
      */
     @Override
-    public List<Order> findList(Map<String, Object> searchMap){
+    public List<Order> findList(Map<String, Object> searchMap) {
         Example example = createExample(searchMap);
         return orderMapper.selectByExample(example);
     }
 
     /**
      * 分页查询
+     *
      * @param page
      * @param size
      * @return
      */
     @Override
-    public Page<Order> findPage(int page, int size){
-        PageHelper.startPage(page,size);
-        return (Page<Order>)orderMapper.selectAll();
+    public Page<Order> findPage(int page, int size) {
+        PageHelper.startPage(page, size);
+        return (Page<Order>) orderMapper.selectAll();
     }
 
     /**
      * 条件+分页查询
+     *
      * @param searchMap 查询条件
-     * @param page 页码
-     * @param size 页大小
+     * @param page      页码
+     * @param size      页大小
      * @return 分页结果
      */
     @Override
-    public Page<Order> findPage(Map<String,Object> searchMap, int page, int size){
-        PageHelper.startPage(page,size);
+    public Page<Order> findPage(Map<String, Object> searchMap, int page, int size) {
+        PageHelper.startPage(page, size);
         Example example = createExample(searchMap);
-        return (Page<Order>)orderMapper.selectByExample(example);
+        return (Page<Order>) orderMapper.selectByExample(example);
     }
 
     @Autowired
@@ -199,7 +207,7 @@ public class OrderServiceImpl implements OrderService {
 
         //1.查询订单
         Order order = orderMapper.selectByPrimaryKey(orderId);
-        if (order != null && "0".equals(order.getPayStatus())){
+        if (order != null && "0".equals(order.getPayStatus())) {
             //2.修改订单的支付状态
             order.setPayStatus("1");
             order.setOrderStatus("1");
@@ -210,16 +218,15 @@ public class OrderServiceImpl implements OrderService {
 
             //3.记录订单日志
             OrderLog orderLog = new OrderLog();
-            orderLog.setId(idWorker.nextId()+"");
+            orderLog.setId(idWorker.nextId() + "");
             orderLog.setOperater("system");
             orderLog.setOperateTime(new Date());
             orderLog.setOrderStatus("1");
             orderLog.setPayStatus("1");
-            orderLog.setRemarks("交易流水号:"+transactionId);
+            orderLog.setRemarks("交易流水号:" + transactionId);
             orderLog.setOrderId(orderId);
             orderLogMapper.insert(orderLog);
         }
-
 
     }
 
@@ -235,29 +242,29 @@ public class OrderServiceImpl implements OrderService {
          * 2.1)如果当前订单的支付状态为已支付,则进行数据补偿(mysql)
          * 2.2)如果当前订单的支付状态为未支付,则修改mysql中的订单信息,新增订单日志,恢复商品的库存,基于微信关闭订单
          */
-        System.out.println("关闭订单业务开启:"+orderId);
+        System.out.println("关闭订单业务开启:" + orderId);
         Order order = orderMapper.selectByPrimaryKey(orderId);
-        if (order == null){
+        if (order == null) {
             throw new RuntimeException("订单不存在!");
         }
-        if (!"0".equals(order.getPayStatus())){
+        if (!"0".equals(order.getPayStatus())) {
             System.out.println("当前订单不需要关闭");
             return;
         }
-        System.out.println("关闭订单校验通过:"+orderId);
+        System.out.println("关闭订单校验通过:" + orderId);
 
         //基于微信查询订单信息
-       Map wxQueryMap = (Map) payFeign.queryOrder(orderId).getData();
-       System.out.println("查询微信支付订单:"+wxQueryMap);
+        Map wxQueryMap = (Map) payFeign.queryOrder(orderId).getData();
+        System.out.println("查询微信支付订单:" + wxQueryMap);
 
-       //如果订单的支付状态为已支付,进行数据补偿(mysql)
-        if ("SUCCESS".equals(wxQueryMap.get("trade_state"))){
-            this.updatePayStatus(orderId,(String) wxQueryMap.get("transaction_id"));
+        //如果订单的支付状态为已支付,进行数据补偿(mysql)
+        if ("SUCCESS".equals(wxQueryMap.get("trade_state"))) {
+            this.updatePayStatus(orderId, (String) wxQueryMap.get("transaction_id"));
             System.out.println("完成数据补偿");
         }
 
         //如果订单的支付状态为未支付,则修改mysql中的订单信息,新增订单日志,恢复商品的库存,基于微信关闭订单
-        if ("NOTPAY".equals(wxQueryMap.get("trade_state"))){
+        if ("NOTPAY".equals(wxQueryMap.get("trade_state"))) {
             System.out.println("执行关闭");
             order.setUpdateTime(new Date());
             order.setOrderStatus("4"); //订单已关闭
@@ -265,7 +272,7 @@ public class OrderServiceImpl implements OrderService {
 
             //新增订单日志
             OrderLog orderLog = new OrderLog();
-            orderLog.setId(idWorker.nextId()+"");
+            orderLog.setId(idWorker.nextId() + "");
             orderLog.setOperater("system");
             orderLog.setOperateTime(new Date());
             orderLog.setOrderStatus("4");
@@ -278,7 +285,7 @@ public class OrderServiceImpl implements OrderService {
             List<OrderItem> orderItemList = orderItemMapper.select(_orderItem);
 
             for (OrderItem orderItem : orderItemList) {
-                skuFeign.resumeStockNum(orderItem.getSkuId(),orderItem.getNum());
+                skuFeign.resumeStockNum(orderItem.getSkuId(), orderItem.getNum());
             }
 
             //基于微信关闭订单
@@ -294,10 +301,10 @@ public class OrderServiceImpl implements OrderService {
 
         //判断每一个订单的运单号和物流公司的值是否存在
         for (Order order : orders) {
-            if (order.getId() == null){
+            if (order.getId() == null) {
                 throw new RuntimeException("订单号不存在!");
             }
-            if (order.getShippingCode() == null || order.getShippingName() == null){
+            if (order.getShippingCode() == null || order.getShippingName() == null) {
                 throw new RuntimeException("请输入运单号或物流公司的名称");
             }
         }
@@ -305,7 +312,7 @@ public class OrderServiceImpl implements OrderService {
         //进行订单状态的校验
         for (Order order : orders) {
             Order order1 = orderMapper.selectByPrimaryKey(order.getId());
-            if (!"0".equals(order1.getConsignStatus()) || !"1".equals(order1.getOrderStatus())){
+            if (!"0".equals(order1.getConsignStatus()) || !"1".equals(order1.getOrderStatus())) {
                 throw new RuntimeException("订单状态不合法");
             }
         }
@@ -320,7 +327,7 @@ public class OrderServiceImpl implements OrderService {
 
             //记录订单日志
             OrderLog orderLog = new OrderLog();
-            orderLog.setId(idWorker.nextId()+"");
+            orderLog.setId(idWorker.nextId() + "");
             orderLog.setOperateTime(new Date());
             orderLog.setOperater("admin");
             orderLog.setOrderStatus("2");
@@ -335,10 +342,10 @@ public class OrderServiceImpl implements OrderService {
     public void confirmTask(String orderId, String operator) {
 
         Order order = orderMapper.selectByPrimaryKey(orderId);
-        if (order == null){
+        if (order == null) {
             throw new RuntimeException("订单不存在");
         }
-        if (!"1".equals(order.getConsignStatus())){
+        if (!"1".equals(order.getConsignStatus())) {
             throw new RuntimeException("订单未发货");
         }
 
@@ -350,7 +357,7 @@ public class OrderServiceImpl implements OrderService {
 
         //记录订单日志
         OrderLog orderLog = new OrderLog();
-        orderLog.setId(idWorker.nextId()+"");
+        orderLog.setId(idWorker.nextId() + "");
         orderLog.setOperateTime(new Date());
         orderLog.setOperater(operator);
         orderLog.setOrderStatus("3");
@@ -381,109 +388,110 @@ public class OrderServiceImpl implements OrderService {
         //按条件查询,获取订单列表
         Example example = new Example(Order.class);
         Example.Criteria criteria = example.createCriteria();
-        criteria.andLessThan("consignTime",date);
-        criteria.andEqualTo("orderStatus","2");
+        criteria.andLessThan("consignTime", date);
+        criteria.andEqualTo("orderStatus", "2");
         List<Order> orderList = orderMapper.selectByExample(example);
 
         for (Order order : orderList) {
-            this.confirmTask(order.getId(),"system");
+            this.confirmTask(order.getId(), "system");
         }
 
     }
 
     /**
      * 构建查询对象
+     *
      * @param searchMap
      * @return
      */
-    private Example createExample(Map<String, Object> searchMap){
-        Example example=new Example(Order.class);
+    private Example createExample(Map<String, Object> searchMap) {
+        Example example = new Example(Order.class);
         Example.Criteria criteria = example.createCriteria();
-        if(searchMap!=null){
+        if (searchMap != null) {
             // 订单id
-            if(searchMap.get("id")!=null && !"".equals(searchMap.get("id"))){
-                criteria.andEqualTo("id",searchMap.get("id"));
-           	}
+            if (searchMap.get("id") != null && !"".equals(searchMap.get("id"))) {
+                criteria.andEqualTo("id", searchMap.get("id"));
+            }
             // 支付类型，1、在线支付、0 货到付款
-            if(searchMap.get("payType")!=null && !"".equals(searchMap.get("payType"))){
-                criteria.andEqualTo("payType",searchMap.get("payType"));
-           	}
+            if (searchMap.get("payType") != null && !"".equals(searchMap.get("payType"))) {
+                criteria.andEqualTo("payType", searchMap.get("payType"));
+            }
             // 物流名称
-            if(searchMap.get("shippingName")!=null && !"".equals(searchMap.get("shippingName"))){
-                criteria.andLike("shippingName","%"+searchMap.get("shippingName")+"%");
-           	}
+            if (searchMap.get("shippingName") != null && !"".equals(searchMap.get("shippingName"))) {
+                criteria.andLike("shippingName", "%" + searchMap.get("shippingName") + "%");
+            }
             // 物流单号
-            if(searchMap.get("shippingCode")!=null && !"".equals(searchMap.get("shippingCode"))){
-                criteria.andLike("shippingCode","%"+searchMap.get("shippingCode")+"%");
-           	}
+            if (searchMap.get("shippingCode") != null && !"".equals(searchMap.get("shippingCode"))) {
+                criteria.andLike("shippingCode", "%" + searchMap.get("shippingCode") + "%");
+            }
             // 用户名称
-            if(searchMap.get("username")!=null && !"".equals(searchMap.get("username"))){
-                criteria.andLike("username","%"+searchMap.get("username")+"%");
-           	}
+            if (searchMap.get("username") != null && !"".equals(searchMap.get("username"))) {
+                criteria.andLike("username", "%" + searchMap.get("username") + "%");
+            }
             // 买家留言
-            if(searchMap.get("buyerMessage")!=null && !"".equals(searchMap.get("buyerMessage"))){
-                criteria.andLike("buyerMessage","%"+searchMap.get("buyerMessage")+"%");
-           	}
+            if (searchMap.get("buyerMessage") != null && !"".equals(searchMap.get("buyerMessage"))) {
+                criteria.andLike("buyerMessage", "%" + searchMap.get("buyerMessage") + "%");
+            }
             // 是否评价
-            if(searchMap.get("buyerRate")!=null && !"".equals(searchMap.get("buyerRate"))){
-                criteria.andLike("buyerRate","%"+searchMap.get("buyerRate")+"%");
-           	}
+            if (searchMap.get("buyerRate") != null && !"".equals(searchMap.get("buyerRate"))) {
+                criteria.andLike("buyerRate", "%" + searchMap.get("buyerRate") + "%");
+            }
             // 收货人
-            if(searchMap.get("receiverContact")!=null && !"".equals(searchMap.get("receiverContact"))){
-                criteria.andLike("receiverContact","%"+searchMap.get("receiverContact")+"%");
-           	}
+            if (searchMap.get("receiverContact") != null && !"".equals(searchMap.get("receiverContact"))) {
+                criteria.andLike("receiverContact", "%" + searchMap.get("receiverContact") + "%");
+            }
             // 收货人手机
-            if(searchMap.get("receiverMobile")!=null && !"".equals(searchMap.get("receiverMobile"))){
-                criteria.andLike("receiverMobile","%"+searchMap.get("receiverMobile")+"%");
-           	}
+            if (searchMap.get("receiverMobile") != null && !"".equals(searchMap.get("receiverMobile"))) {
+                criteria.andLike("receiverMobile", "%" + searchMap.get("receiverMobile") + "%");
+            }
             // 收货人地址
-            if(searchMap.get("receiverAddress")!=null && !"".equals(searchMap.get("receiverAddress"))){
-                criteria.andLike("receiverAddress","%"+searchMap.get("receiverAddress")+"%");
-           	}
+            if (searchMap.get("receiverAddress") != null && !"".equals(searchMap.get("receiverAddress"))) {
+                criteria.andLike("receiverAddress", "%" + searchMap.get("receiverAddress") + "%");
+            }
             // 订单来源：1:web，2：app，3：微信公众号，4：微信小程序  5 H5手机页面
-            if(searchMap.get("sourceType")!=null && !"".equals(searchMap.get("sourceType"))){
-                criteria.andEqualTo("sourceType",searchMap.get("sourceType"));
-           	}
+            if (searchMap.get("sourceType") != null && !"".equals(searchMap.get("sourceType"))) {
+                criteria.andEqualTo("sourceType", searchMap.get("sourceType"));
+            }
             // 交易流水号
-            if(searchMap.get("transactionId")!=null && !"".equals(searchMap.get("transactionId"))){
-                criteria.andLike("transactionId","%"+searchMap.get("transactionId")+"%");
-           	}
+            if (searchMap.get("transactionId") != null && !"".equals(searchMap.get("transactionId"))) {
+                criteria.andLike("transactionId", "%" + searchMap.get("transactionId") + "%");
+            }
             // 订单状态
-            if(searchMap.get("orderStatus")!=null && !"".equals(searchMap.get("orderStatus"))){
-                criteria.andEqualTo("orderStatus",searchMap.get("orderStatus"));
-           	}
+            if (searchMap.get("orderStatus") != null && !"".equals(searchMap.get("orderStatus"))) {
+                criteria.andEqualTo("orderStatus", searchMap.get("orderStatus"));
+            }
             // 支付状态
-            if(searchMap.get("payStatus")!=null && !"".equals(searchMap.get("payStatus"))){
-                criteria.andEqualTo("payStatus",searchMap.get("payStatus"));
-           	}
+            if (searchMap.get("payStatus") != null && !"".equals(searchMap.get("payStatus"))) {
+                criteria.andEqualTo("payStatus", searchMap.get("payStatus"));
+            }
             // 发货状态
-            if(searchMap.get("consignStatus")!=null && !"".equals(searchMap.get("consignStatus"))){
-                criteria.andEqualTo("consignStatus",searchMap.get("consignStatus"));
-           	}
+            if (searchMap.get("consignStatus") != null && !"".equals(searchMap.get("consignStatus"))) {
+                criteria.andEqualTo("consignStatus", searchMap.get("consignStatus"));
+            }
             // 是否删除
-            if(searchMap.get("isDelete")!=null && !"".equals(searchMap.get("isDelete"))){
-                criteria.andEqualTo("isDelete",searchMap.get("isDelete"));
-           	}
+            if (searchMap.get("isDelete") != null && !"".equals(searchMap.get("isDelete"))) {
+                criteria.andEqualTo("isDelete", searchMap.get("isDelete"));
+            }
 
             // 数量合计
-            if(searchMap.get("totalNum")!=null ){
-                criteria.andEqualTo("totalNum",searchMap.get("totalNum"));
+            if (searchMap.get("totalNum") != null) {
+                criteria.andEqualTo("totalNum", searchMap.get("totalNum"));
             }
             // 金额合计
-            if(searchMap.get("totalMoney")!=null ){
-                criteria.andEqualTo("totalMoney",searchMap.get("totalMoney"));
+            if (searchMap.get("totalMoney") != null) {
+                criteria.andEqualTo("totalMoney", searchMap.get("totalMoney"));
             }
             // 优惠金额
-            if(searchMap.get("preMoney")!=null ){
-                criteria.andEqualTo("preMoney",searchMap.get("preMoney"));
+            if (searchMap.get("preMoney") != null) {
+                criteria.andEqualTo("preMoney", searchMap.get("preMoney"));
             }
             // 邮费
-            if(searchMap.get("postFee")!=null ){
-                criteria.andEqualTo("postFee",searchMap.get("postFee"));
+            if (searchMap.get("postFee") != null) {
+                criteria.andEqualTo("postFee", searchMap.get("postFee"));
             }
             // 实付金额
-            if(searchMap.get("payMoney")!=null ){
-                criteria.andEqualTo("payMoney",searchMap.get("payMoney"));
+            if (searchMap.get("payMoney") != null) {
+                criteria.andEqualTo("payMoney", searchMap.get("payMoney"));
             }
 
         }
